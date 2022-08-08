4896d1251adc22510f19c0b33af03030dd457e83
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-08 22:03:11 +0200

    Create MyApplication Class

---
+++ app/src/main/java/com/seif/banquemisrttask/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.seif.banquemisrttask
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application()
+// this is the place where dagger component should be generated automatically for us
