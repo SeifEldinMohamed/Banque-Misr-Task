738e42cee57f172d1e271424d45e61c81793f36b
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-08 22:33:54 +0200

    Inject RemoteDataSource Class With TrendingRepositoriesApI

---
+++ app/src/main/java/com/seif/banquemisrttask/data/network/RemoteDataSource.kt
@@ -0,0 +1,14 @@
+package com.seif.banquemisrttask.data.network
+
+import com.seif.banquemisrttask.data.database.entities.TrendingRepositories
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val trendingRepositoriesApi: TrendingRepositoriesApi
+) { // fetch data from our api
+
+   suspend fun getTrendingRepositories(): Response<TrendingRepositories> {
+        return trendingRepositoriesApi.getTrendingRepositories()
+    }
+}
