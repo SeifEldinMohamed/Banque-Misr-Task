af74414d87491412f9accc7f4367c14d48775820
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-08 23:56:02 +0200

    Create Main Menu

--- app/src/main/java/com/seif/banquemisrttask/ui/TrendingActivity.kt
+++ app/src/main/java/com/seif/banquemisrttask/ui/TrendingActivity.kt
@@ -2,7 +2,12 @@ package com.seif.banquemisrttask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.seif.banquemisrttask.R
 import com.seif.banquemisrttask.databinding.TrendingMainBinding
+import kotlinx.coroutines.withTimeout
 
 class TrendingActivity : AppCompatActivity() {
     lateinit var binding: TrendingMainBinding
@@ -11,5 +16,24 @@ class TrendingActivity : AppCompatActivity() {
         binding = TrendingMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.shimmerFrameLayout.startShimmer()
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = ""
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_sort_by_name -> {
+            }
+            R.id.menu_sort_by_stars -> {
+
+            }
+        }
+        return true
     }
 }
