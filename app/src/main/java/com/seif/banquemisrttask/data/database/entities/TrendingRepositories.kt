894973c91455e849c7c926ca76ab185319786460
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-08 21:30:37 +0200

    Generate Model Classes For API

---
+++ app/src/main/java/com/seif/banquemisrttask/data/database/entities/TrendingRepositories.kt
@@ -0,0 +1,6 @@
+package com.seif.banquemisrttask.data.database.entities
+
+
+import com.google.gson.annotations.SerializedName
+
+class TrendingRepositories : ArrayList<TrendingRepositoriesItem>()
---
+++ app/src/main/java/com/seif/banquemisrttask/data/database/entities/TrendingRepositoriesItem.kt
@@ -0,0 +1,25 @@
+package com.seif.banquemisrttask.data.database.entities
+
+
+import com.google.gson.annotations.SerializedName
+
+data class TrendingRepositoriesItem(
+    @SerializedName("author")
+    val author: String,
+    @SerializedName("avatar")
+    val avatar: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("forks")
+    val forks: Int,
+    @SerializedName("language")
+    val language: String,
+    @SerializedName("languageColor")
+    val languageColor: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("stars")
+    val stars: Int,
+    @SerializedName("url")
+    val url: String
+)
