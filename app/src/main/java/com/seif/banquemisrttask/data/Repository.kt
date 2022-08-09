0789be2ffc7723751b9f6adc524810700228679f
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-09 18:52:15 +0200

    Create and Inject LocalDataSource

--- app/src/main/java/com/seif/banquemisrttask/data/Repository.kt
+++ app/src/main/java/com/seif/banquemisrttask/data/Repository.kt
@@ -1,12 +1,15 @@
 package com.seif.banquemisrttask.data
 
+import com.seif.banquemisrttask.data.database.LocalDataSource
 import com.seif.banquemisrttask.data.network.RemoteDataSource
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
 @ActivityRetainedScoped // we will have the same instance even when we rotate screen
 class Repository @Inject constructor(
-    remoteDataSource: RemoteDataSource
+    remoteDataSource: RemoteDataSource,
+    localDataSource: LocalDataSource
 ) {
     val remote = remoteDataSource
+    val local = localDataSource
 }
--- app/src/main/java/com/seif/banquemisrttask/data/database/TrendingRepositoriesDao.kt
+++ app/src/main/java/com/seif/banquemisrttask/data/database/TrendingRepositoriesDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.seif.banquemisrttask.data.database.entities.TrendingRepositoriesEntity
 import com.seif.banquemisrttask.data.network.models.TrendingRepositories
 import kotlinx.coroutines.flow.Flow
 
@@ -11,8 +12,8 @@ import kotlinx.coroutines.flow.Flow
 interface TrendingRepositoriesDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertTrendingRepositories(trendingRepositories: TrendingRepositories)
+    suspend fun insertTrendingRepositories(trendingRepositoriesEntity: TrendingRepositoriesEntity)
 
     @Query("SELECT * FROM TRENDING_TABLE ORDER BY id")
-    fun readTrendingRepositories(): Flow<List<TrendingRepositories>>
+    fun readTrendingRepositories(): Flow<List<TrendingRepositoriesEntity>>
 }
