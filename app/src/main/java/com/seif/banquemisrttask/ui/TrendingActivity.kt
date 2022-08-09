bef9843f83b28345d4134aed959d27c9d1d6bcf7
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-09 13:59:51 +0200

    Calling API From Main Activity

--- app/src/main/java/com/seif/banquemisrttask/ui/TrendingActivity.kt
+++ app/src/main/java/com/seif/banquemisrttask/ui/TrendingActivity.kt
@@ -2,21 +2,69 @@ package com.seif.banquemisrttask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.seif.banquemisrttask.R
 import com.seif.banquemisrttask.databinding.TrendingMainBinding
+import com.seif.banquemisrttask.ui.adapters.TrendingRepositoriesAdapter
+import com.seif.banquemisrttask.util.NetworkResult
+import com.seif.banquemisrttask.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TrendingActivity : AppCompatActivity() {
     lateinit var binding: TrendingMainBinding
+    private val trendingAdapter: TrendingRepositoriesAdapter by lazy { TrendingRepositoriesAdapter() }
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = TrendingMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.shimmerFrameLayout.startShimmer()
+
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setSupportActionBar(binding.toolbar)
         supportActionBar?.title = ""
 
+        setUpRecyclerView()
+        requestApiData()
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getTrendingRepositories()
+        mainViewModel.trendingRepositoriesResponse.observe(this) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    showRecyclerViewAndHideShimmerEffect()
+                    response.data?.let {
+                        trendingAdapter.addTrendingRepositories(it)
+                        Log.d("main",it.toString())
+                    }
+                }
+                is NetworkResult.Error -> {
+                     showRecyclerViewAndHideShimmerEffect()
+                    // show retry button to user
+                    Snackbar.make(binding.root, response.message.toString(), Snackbar.LENGTH_LONG)
+                        .show()
+                    Log.d("main",response.message.toString())
+                }
+                is NetworkResult.Loading -> {
+                    showShimmerEffectAndHideRecyclerView()
+                }
+            }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        binding.rvTrending.apply {
+            layoutManager = LinearLayoutManager(this@TrendingActivity)
+            adapter = trendingAdapter
+        }
+        showShimmerEffectAndHideRecyclerView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -25,7 +73,7 @@ class TrendingActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.menu_sort_by_name -> {
             }
             R.id.menu_sort_by_stars -> {
@@ -33,5 +81,21 @@ class TrendingActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    private fun showRecyclerViewAndHideShimmerEffect() {
+        binding.shimmerFrameLayout.apply {
+            visibility = View.GONE
+            stopShimmer()
+        }
+        binding.rvTrending.visibility = View.VISIBLE
+    }
+
+    private fun showShimmerEffectAndHideRecyclerView() {
+        binding.shimmerFrameLayout.apply {
+            visibility = View.VISIBLE
+            startShimmer()
+        }
+        binding.rvTrending.visibility = View.GONE
     }
 }
