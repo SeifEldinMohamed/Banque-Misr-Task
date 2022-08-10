5cedab5b115162da7128e2bd74a4cd8d2fd87bb0
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-10 22:14:09 +0200

    Testing Architecture Skeleton

---
+++ app/src/test/java/com/seif/banquemisrttask/repositories/FakeTrendingRepository.kt
@@ -0,0 +1,65 @@
+package com.seif.banquemisrttask.repositories
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import com.seif.banquemisrttask.data.database.entities.TrendingRepositoriesEntity
+import com.seif.banquemisrttask.data.network.models.TrendingRepositories
+import com.seif.banquemisrttask.data.network.models.TrendingRepositoriesItem
+import com.seif.banquemisrttask.util.NetworkResult
+import kotlinx.coroutines.flow.Flow
+
+
+class FakeTrendingRepository { // make this class to test viewModel
+
+    private val trendingRepositoriesItems = mutableListOf<TrendingRepositoriesEntity>()
+
+    private val observeTrendingRepositoriesItem =
+        MutableLiveData<List<TrendingRepositoriesEntity>>(trendingRepositoriesItems)
+
+    private var shouldReturnNetworkError: Boolean = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLifeData() {
+        observeTrendingRepositoriesItem.postValue(trendingRepositoriesItems)
+    }
+
+    // remote (Api)
+    suspend fun getTrendingRepositories(): NetworkResult<TrendingRepositories> {
+
+        return if(shouldReturnNetworkError){ // if we want to return an error
+            NetworkResult.Error("error")
+        }
+        else{
+            val trendingRepositoriesList = arrayListOf<TrendingRepositoriesItem>(
+                TrendingRepositoriesItem(
+                    "seif",
+                    "https://picsum.photos/seed/picsum/200/300",
+                    "hello, this seif's repository",
+                    1000,
+                    "Kotlin",
+                    "#000000",
+                    "My Kotlin Repository",
+                    2000,
+                    "https://picsum.photos/seed/picsum/200/300"
+                ))
+            val trendingRepositories = TrendingRepositories()
+            trendingRepositories.addAll(trendingRepositoriesList)
+
+            NetworkResult.Success(trendingRepositories)
+        }
+    }
+
+    // local (ROOM)
+    fun readTrendingRepositories(): Flow<List<TrendingRepositoriesEntity>> {
+        return observeTrendingRepositoriesItem.asFlow()
+    }
+
+    suspend fun insertTrendingRepositories(trendingRepositoriesEntity: TrendingRepositoriesEntity) {
+        trendingRepositoriesItems.add(trendingRepositoriesEntity)
+        refreshLifeData()
+    }
+
+}
