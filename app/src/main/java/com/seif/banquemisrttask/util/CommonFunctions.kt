9e79505a6254f2ce64ac4259797b157c40076f6c
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-24 21:40:48 +0200

    Refactor Code 2

--- app/src/main/java/com/seif/banquemisrttask/util/CommonFunctions.kt
+++ app/src/main/java/com/seif/banquemisrttask/util/CommonFunctions.kt
@@ -1,31 +1,11 @@
 package com.seif.banquemisrttask.util
 
-import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 class CommonFunctions {
-
-//        fun checkInternetConnection (): Boolean{
-//            val connectivityManager = context.getSystemService(
-//                Context.CONNECTIVITY_SERVICE
-//            ) as ConnectivityManager
-//
-//            val activeNetwork: Network = connectivityManager.activeNetwork ?: return false
-//            val capabilities: NetworkCapabilities =
-//                connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
-//
-//            return when { // return true if there is an internet connection from wifi, cellular and ethernet
-//                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-//                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-//                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-//                else -> false
-//            }
-//        }
 
     @Inject
     lateinit var connectivityManager: ConnectivityManager
